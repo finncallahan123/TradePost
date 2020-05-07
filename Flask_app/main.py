d498ce84fcf87baab9678122dce07146fc0a5df8
finncallahan123 <finnc129@gmail.com>
2020-05-07 10:02:29 -0400

    Fix routes

--- Flask_app/main.py
+++ Flask_app/main.py
@@ -3,23 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/Home')
-def Home():
-
-    return render_template('Home.html')
-
 @app.route("/Register2")
-def Register2():
+def register():
 
     # form = RegistrationForm()
     # if form.validate_on_submit():
     #     flash(f'Account created for {form.username.data}!', 'success')
     #     return redirect(url_for('login'))
 
-    return render_template('Register2.html')
+    return render_template('register.html')
 @app.route('/Login2')
-def Login2():
+def login():
     # form = LoginForm()
     # if form.validate_on_submit():
     #     if form.email.data=="bob@bob.com" and form.password.data=="bobby":
@@ -27,10 +21,9 @@ def Login2():
     #         return redirect(url_for('timeDisplay'))
     #     else: flash('Please check your username and password')
 
-    return render_template('Login2.html')
+    return render_template('login.html')
 
+@app.route('/Home')
+def home():
 
-@app.route('/Feed')
-def Feed():
-
-    return render_template('IndexTP.html')
+    return render_template('home.html')
